70bf241415cb0e34ec9b6b1f55e9ba15adcaa939
Nguyễn Văn Quyết <121377981+Paul1622005@users.noreply.github.com>
2024-01-21 12:03:21 +0700

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,10 +1,7 @@
-const container = document.getElementById
-('container');
-const registerBtn = document.
-getElementById('register');
-const loginBtn = document.
-getElementById('login');
-
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+// click button 
 registerBtn.addEventListener('click', () =>{
     container.classList.add("active");
 });
