557fad159e58271028d4f036a9ea752c4bcc8bd8
Nguyễn Văn Quyết <121377981+Paul1622005@users.noreply.github.com>
2024-01-24 18:43:48 +0700

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,9 +1,11 @@
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
-// click button 
-registerBtn.addEventListener('click', () =>{  container.classList.add("active");
+// click
+registerBtn.addEventListener('click', () =>{ 
+  container.classList.add("active");
 });
-loginBtn.addEventListener('click', () =>{  container.classList.remove("active");
+loginBtn.addEventListener('click', () =>{ 
+  container.classList.remove("active");
 });
 
